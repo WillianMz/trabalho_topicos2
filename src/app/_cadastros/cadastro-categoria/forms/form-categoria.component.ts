b12de8ed704f2c97ff1888c772bb9f0d1e016e49
Tiago Graciosa Dorneles <tiago.g.dorneles@gmail.com>
2021-06-08 21:31:43 -0300

    Validações cadastro categoria

--- src/app/_cadastros/cadastro-categoria/forms/form-categoria.component.ts
+++ src/app/_cadastros/cadastro-categoria/forms/form-categoria.component.ts
@@ -21,39 +21,39 @@ export class FormCategoriaComponent implements OnInit {
     private categoriaService: CategoriaService,
     private router: Router,
   ) {
-    
     let categoria = {
       id: null,
       nome: '',
-      url_img: ''
+      url_img: '',
     };
     this.inicializaFormulario(categoria);
    }
 
    ngOnInit() {
-     const id = this.activatedRoute.snapshot.paramMap.get('id');
-     if (id) {
-        this.categoriaId = parseInt(id);
-        this.categoriaService
-         .getCategoriaById(this.categoriaId)
-         .subscribe((categoria) => {
-           this.inicializaFormulario(categoria);
-         },
-         (erro) => {
-          console.error(erro);
-        }
-         );
-     }
-     
-   }
+    console.log('ngOnInit');
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+       this.categoriaId = parseInt(id);
+       this.categoriaService
+        .getCategoriaById(this.categoriaId)
+        .subscribe((categoria) => {
+          this.inicializaFormulario(categoria);
+        });
+    }
+  }
 
   inicializaFormulario(categoria: Categoria) {
     this.categoriaForm = new FormGroup({
-      nome: new FormControl(categoria.nome),
-      url_img: new FormControl(categoria.url_img)
-    });
-    console.log(this.categoriaForm);
-    
+      nome: new FormControl(categoria.nome, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(150)
+      ]),
+      url_img: new FormControl(categoria.url_img, [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+    })
   }
 
   salvar() {
@@ -75,6 +75,10 @@ export class FormCategoriaComponent implements OnInit {
 
   get nome() {
     return this.categoriaForm.get('nome');
+  }
+
+  get url_img() {
+    return this.categoriaForm.get('url_img');
   }
 
 }
